75f7bc212b0f70493708ab9fc9f715eaa8af0b76
Keith Dahlby <keith@newbo.co>
2019-12-17 17:07:50 -0600

    Merge job into LocationDelegates

--- Samples/ShinyDelegates/GeofenceBackgroundJob.cs
+++ Samples/ShinyDelegates/GeofenceBackgroundJob.cs
@@ -1,51 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Shiny.Jobs;
-using Shiny.Logging;
-
-namespace Samples.ShinyDelegates
+﻿namespace Samples.ShinyDelegates
 {
-    public class GeofenceBackgroundJob : IJob
+    public class GeofenceBackgroundJob : LocationDelegates
     {
-        readonly HttpClient httpClient = new HttpClient();
-        readonly CoreDelegateServices services;
-
-        public GeofenceBackgroundJob(CoreDelegateServices services)
+        public GeofenceBackgroundJob(CoreDelegateServices services) : base(services)
         {
-            this.services = services;
-        }
-
-        public async Task<bool> Run(JobInfo jobInfo, CancellationToken cancelToken)
-        {
-            var events = await services.Connection.GeofenceEvents.Where(x => x.Reported == null).ToListAsync();
-            Log.Write(jobInfo.Identifier, $"Unreported: {events.Count}");
-
-            if (events.Count == 0)
-                return true;
-
-            foreach (var group in events.GroupBy(e => e.Identifier))
-            {
-                var latest = group.OrderByDescending(e => e.Date).First();
-                string eventType = latest.Entered ? "entered" : "exited";
-                var text = $"Group {group.Key} {eventType} at {latest.Date:h:mm tt}";
-                Log.Write(jobInfo.Identifier, text);
-
-                var response = await httpClient.PostAsync(Constants.SlackWebhook,
-                    new StringContent(JsonConvert.SerializeObject(new { text }), null, "application/json"),
-                    cancelToken);
-
-                var now = DateTime.Now;
-                foreach (var ge in group)
-                    ge.Reported = now;
-
-                await services.Connection.UpdateAllAsync(group);
-            }
-
-            return true;
         }
     }
 }
--- Samples/ShinyDelegates/LocationDelegates.cs
+++ Samples/ShinyDelegates/LocationDelegates.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Samples.Models;
+using Shiny.Jobs;
 using Shiny.Locations;
+using Shiny.Logging;
 
 namespace Samples.ShinyDelegates
 {
-    public class LocationDelegates : IGeofenceDelegate, IGpsDelegate
+    public class LocationDelegates : IGeofenceDelegate, IGpsDelegate, IJob
     {
+        readonly HttpClient httpClient = new HttpClient();
         readonly CoreDelegateServices services;
         public LocationDelegates(CoreDelegateServices services) => this.services = services;
 
@@ -42,6 +49,42 @@ namespace Samples.ShinyDelegates
                 $"{region.Identifier} was {newStatus}",
                 x => notify
             );
+
+            await ReportEvents("OnStatusChanged", default);
+        }
+
+        public async Task<bool> Run(JobInfo jobInfo, CancellationToken cancelToken)
+        {
+            await ReportEvents(jobInfo.Identifier, cancelToken);
+            return true;
+        }
+
+        public async Task ReportEvents(string eventName, CancellationToken cancelToken)
+        {
+            var events = await services.Connection.GeofenceEvents.ToListAsync();
+            var unreported = events.Count(e => e.Reported == null);
+            Log.Write(eventName, $"Unreported: {unreported}");
+
+            if (unreported == 0)
+                return;
+
+            foreach (var group in events.GroupBy(e => e.Identifier))
+            {
+                var latest = group.OrderByDescending(e => e.Date).First();
+                if (latest.Reported != null)
+                    continue;
+
+                string eventType = latest.Entered ? "entered" : "exited";
+                var text = $"Group {group.Key} {eventType} at {latest.Date:h:mm tt}";
+                Log.Write(eventName, text);
+
+                var response = await httpClient.PostAsync(Constants.SlackWebhook,
+                    new StringContent(JsonConvert.SerializeObject(new { text }), null, "application/json"),
+                    cancelToken);
+
+                latest.Reported = DateTime.Now;
+                await services.Connection.UpdateAsync(latest);
+            }
         }
     }
 }
